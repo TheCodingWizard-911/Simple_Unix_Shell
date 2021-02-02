04207dc9e8f3c203a723162e3cd52417bccf98d1
Ganesh Gitte <ganeshs.gitte911@gmail.com>
2021-02-02 12:26:34 +0530

    Execution status to stop loop in case of error

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 int main()
 {
   char *command, **args;
-  int status = 1;
+  int status;
 
   do
   {
     printf("\n$_> ");
     command = readCommand();
     args = splitCommand(command);
+    status = executeCommand(args);
 
     free(command);
     free(args);
